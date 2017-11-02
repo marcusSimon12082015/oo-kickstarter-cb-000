cf43c53896160d73a098ed88a67fff8e095f5192
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 21:32:44 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,6 +6,6 @@ class Backer
   end
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self)
+    project.add_backer(self) unless project.backers.include?(self)
   end
 end
