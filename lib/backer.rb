f60798444906d05eca2598a206cfcf9bc5404178
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-02 21:25:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,5 +2,9 @@ class Backer
   attr_accessor :name
   def initialize(name)
     @name = name
+    @backed_projects = []
+  end
+  def back_project(project)
+    @backed_projects << project
   end
 end
